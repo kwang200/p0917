a83f847d55647e2faf59698d63cd1471af1255e8
www <kwangyeol300@naver.com>
2018-09-17 10:09:45 +0900

    p0917 메시지 헬로월드

---
+++ p0917/src/p0917/Helloworld.java
@@ -0,0 +1,12 @@
+package p0917;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+      System.out.println("hello world");
+      System.out.println("hello worldeeee");
+      System.out.println("hello world222");
+      System.out.println("hello world222");
+	}
+
+}
